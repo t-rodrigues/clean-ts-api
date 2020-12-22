124b76554a4434373dbc31b339b9b71cb403cda2
Thiago Rodrigues <thiagor_@live.com>
2020-12-22 20:39:18 -0300

    test: ensure LogMongoRepository creates an error log on sucess

---
+++ src/infra/db/mongodb/repositories/logs-mongo-repository.spec.ts
@@ -0,0 +1,28 @@
+import { MongoHelper } from '@/infra/db/mongodb';
+import { Collection } from 'mongodb';
+import { LogsMongoRepository } from './logs-mongo-repository';
+
+describe('LogsMongoRepository', async () => {
+  let logErrorCollection: Collection;
+
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    logErrorCollection = await MongoHelper.getCollection('errors');
+    await logErrorCollection.deleteMany({});
+  });
+
+  it('should create an error log on success', async () => {
+    const sut = new LogsMongoRepository();
+    await sut.logError('any_stack');
+    const count = await logErrorCollection.countDocuments();
+
+    expect(count).toBe(1);
+  });
+});
