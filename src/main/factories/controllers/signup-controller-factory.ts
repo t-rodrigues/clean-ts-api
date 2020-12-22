e4fc07b1f633d2646d0bce66fd512a979bc25d20
Thiago Rodrigues <thiagor_@live.com>
2020-12-22 18:59:41 -0300

    feat: ensure LogControllerDecorator calls controller with correct value

--- src/main/factories/controllers/signup-controller-factory.ts
+++ src/main/factories/controllers/signup-controller-factory.ts
@@ -2,11 +2,12 @@ import { EmailValidatorAdapter } from '@/infra/validators';
 import { Controller } from '@/presentation/contracts';
 import { SignUpController } from '@/presentation/controllers/signup';
 import { makeDbAddAccount } from '@/main/factories';
+import { LogControllerDecorator } from '@/main/decorators';
 
 export const makeSignUpController = (): Controller => {
   const emailValidator = new EmailValidatorAdapter();
 
   const controller = new SignUpController(emailValidator, makeDbAddAccount());
 
-  return controller;
+  return new LogControllerDecorator(controller);
 };
