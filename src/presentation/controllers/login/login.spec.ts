2baf0d21c7703c76f8ce8a5df553b2ffdb263c54
Thiago Rodrigues <thiagor_@live.com>
2020-12-22 23:00:09 -0300

    test: ensure LoginController calls EmailValidator with correct email

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,26 +1,40 @@
-// import { HttpRequest } from '@/presentation/contracts';
+import { HttpRequest } from '@/presentation/contracts';
 import { MissingParamError } from '@/presentation/errors';
 import { badRequest } from '@/presentation/helpers';
+import { EmailValidator } from '@/validation/contracts';
 import { LoginController } from './login';
 
 type SutTypes = {
   sut: LoginController;
+  emailValidatorStub: EmailValidator;
+};
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid(email: string): boolean {
+      return true;
+    }
+  }
+
+  return new EmailValidatorStub();
 };
 
 const makeSut = (): SutTypes => {
-  const sut = new LoginController();
+  const emailValidatorStub = makeEmailValidator();
+  const sut = new LoginController(emailValidatorStub);
 
   return {
     sut,
+    emailValidatorStub,
   };
 };
 
-// const makeRequest = (): HttpRequest => ({
-//   body: {
-//     email: 'any_email@mail.com',
-//     password: '123123',
-//   },
-// });
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    email: 'any_email@mail.com',
+    password: '123123',
+  },
+});
 
 describe('LoginController', () => {
   it('should return 400 if no email is provided', async () => {
@@ -49,5 +63,18 @@ describe('LoginController', () => {
     const httpResponse = await sut.handle(httpRequest);
 
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')));
+  });
+
+  it('should call EmailValidator with correct email', async () => {
+    const { sut, emailValidatorStub } = makeSut();
+    const httpRequest = makeFakeRequest();
+
+    const emailValidatorSpy = jest
+      .spyOn(emailValidatorStub, 'isValid')
+      .mockReturnValueOnce(false);
+
+    await sut.handle(httpRequest);
+
+    expect(emailValidatorSpy).toHaveBeenCalledWith(httpRequest.body.email);
   });
 });
