473958e1f6917765461883efaa61abdb57adc6d1
Thiago Rodrigues <thiagor_@live.com>
2020-12-23 18:21:16 -0300

    test: ensure RequiredFieldValidation returns a MissinParamError if validations fails

---
+++ src/validation/validators/required-field-validation.spec.ts
@@ -0,0 +1,11 @@
+import { MissingParamError } from '@/presentation/errors';
+import { RequiredFieldValidation } from './required-field-validation';
+
+describe('RequiredFieldValidation', () => {
+  it('should return a MissinParamError if validation fails', async () => {
+    const sut = new RequiredFieldValidation('field');
+    const error = sut.validate({ name: 'any_name' });
+
+    expect(error).toEqual(new MissingParamError('field'));
+  });
+});
