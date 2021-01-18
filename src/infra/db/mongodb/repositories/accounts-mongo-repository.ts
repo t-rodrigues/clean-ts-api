543c43cb09d48d4ec4498194ae966713def6c6ff
Thiago Rodrigues <thiagor_@live.com>
2021-01-18 14:48:15 -0300

    feat: ensure AccountsMongoRepository updates the account accessToken on updateAccessToken success

--- src/infra/db/mongodb/repositories/accounts-mongo-repository.ts
+++ src/infra/db/mongodb/repositories/accounts-mongo-repository.ts
@@ -1,12 +1,16 @@
 import {
   AddAccountRepository,
   LoadAccountByEmailRepository,
+  UpdateAccessTokenRepository,
 } from '@/application/contracts';
 import { DbAccount, DbAddAccountDTO } from '@/application/dtos';
 import { MongoHelper } from '@/infra/db/mongodb';
 
 export class AccountsMongoRepository
-  implements AddAccountRepository, LoadAccountByEmailRepository {
+  implements
+    AddAccountRepository,
+    LoadAccountByEmailRepository,
+    UpdateAccessTokenRepository {
   async add(accountData: DbAddAccountDTO): Promise<DbAccount> {
     const accountCollection = await MongoHelper.getCollection('accounts');
     const result = await accountCollection.insertOne(accountData);
@@ -20,5 +24,13 @@ export class AccountsMongoRepository
     const account = await accountCollection.findOne({ email });
 
     return account && MongoHelper.map(account);
+  }
+
+  async updateAccessToken(userId: string, token: string): Promise<void> {
+    const accountCollection = await MongoHelper.getCollection('accounts');
+    await accountCollection.updateOne(
+      { _id: userId },
+      { $set: { accessToken: token } },
+    );
   }
 }
