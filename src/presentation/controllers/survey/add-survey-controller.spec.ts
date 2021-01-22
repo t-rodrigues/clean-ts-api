19912dc4def74dbbdc796f3409d02eaa13ce277f
Thiago Rodrigues <thiagor_@live.com>
2021-01-21 22:40:48 -0300

    feat: ensure AddSurveyController returns 500 if AddSurveyUseCase throws

--- src/presentation/controllers/survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey-controller.spec.ts
@@ -1,6 +1,6 @@
 import { AddSurvey, AddSurveyDTO } from '@domain/usecases/survey';
 import { HttpRequest, Validation } from '@presentation/contracts';
-import { badRequest } from '@presentation/helpers';
+import { badRequest, serverError } from '@presentation/helpers';
 
 import { AddSurveyController } from './add-survey-controller';
 
@@ -80,5 +80,16 @@ describe('AddSurveyController', () => {
     await sut.handle(httpRequest);
 
     expect(addSpy).toHaveBeenCalledWith(httpRequest.body);
+  });
+
+  it('should return 500 if AddSurvey throws', async () => {
+    const { sut, addSurveyStub } = makeSut();
+    jest
+      .spyOn(addSurveyStub, 'add')
+      .mockReturnValueOnce(Promise.reject(new Error()));
+
+    const httpResponse = await sut.handle(makeFakeRequest());
+
+    expect(httpResponse).toEqual(serverError(new Error()));
   });
 });
--- src/presentation/controllers/survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey-controller.ts
@@ -5,7 +5,7 @@ import {
   HttpResponse,
   Validation,
 } from '@presentation/contracts';
-import { badRequest } from '@presentation/helpers';
+import { badRequest, serverError } from '@presentation/helpers';
 
 export class AddSurveyController implements Controller {
   constructor(
@@ -14,16 +14,20 @@ export class AddSurveyController implements Controller {
   ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body);
+    try {
+      const error = this.validation.validate(httpRequest.body);
 
-    if (error) {
-      return badRequest(error);
+      if (error) {
+        return badRequest(error);
+      }
+
+      const { question, answers } = httpRequest.body;
+
+      await this.addSurvey.add({ question, answers });
+
+      return null;
+    } catch (error) {
+      return serverError(error);
     }
-
-    const { question, answers } = httpRequest.body;
-
-    await this.addSurvey.add({ question, answers });
-
-    return null;
   }
 }
