25401dec10ca94219a6a3c94d01da21a45948fad
Thiago Rodrigues <thiagor_@live.com>
2021-02-12 18:41:39 -0300

    fix: ensure AccountsMongoRepository allows admin to access any route

--- src/infra/db/mongodb/repositories/accounts-mongo-repository.ts
+++ src/infra/db/mongodb/repositories/accounts-mongo-repository.ts
@@ -32,7 +32,12 @@ export class AccountsMongoRepository
     const accountCollection = await MongoHelper.getCollection('accounts');
     const account = await accountCollection.findOne({
       accessToken: token,
-      role,
+      $or: [
+        { role },
+        {
+          role: 'admin',
+        },
+      ],
     });
 
     return account && MongoHelper.map(account);
